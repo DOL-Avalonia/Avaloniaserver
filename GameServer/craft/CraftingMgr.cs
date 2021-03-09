83b76a049ad9dbcab6f25e732e6581115a9d1667
DOL-Avalonia <73434612+DOL-Avalonia@users.noreply.github.com>
2021-03-10 03:31:35 +0700

    Update CraftingMgr.cs
    Add option to adjust Recipe.Product.Price

--- GameServer/craft/CraftingMgr.cs
+++ GameServer/craft/CraftingMgr.cs
@@ -16,6 +16,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using DOL.Database;
 using log4net;
@@ -46,7 +48,7 @@ namespace DOL.GS
     }
 
     /// <summary>
-    /// Description résumée de CraftingMgr.
+    /// Description rÃ©sumÃ©e de CraftingMgr.
     /// </summary>
     public class CraftingMgr
     {
